ec87bfeea295a813aaedcc0358f25c50276a150c
Ennovosoft <ennovosoft@gmail.com>
2021-02-04 20:02:11 +0600

    installed both frontend and backend

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# React_Laravel_Blog
+# React Laravel Blog
+
+# Install Frontend
+
+<span>1. cd frontend</span>
+<span>2. npm install</span>
+<span>3. npm start</span>
+
+# Install Backend
+
+<span>1. cd backend</span>
+<span>2. composer install</span>
+<span>3. php artisan serve</span>
---
+++ backend/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel API</title>
+</head>
+
+<body class="antialiased">
+    Only for API Development
+</body>
+
+</html>
